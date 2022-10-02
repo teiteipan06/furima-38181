a9db3dd3d6495ae334f65169282688ab0ae6b6d3
teiteipan06 <y7321543@gmail.com>
2022-10-02 16:44:46 +0900

    furima controllerの削除

--- app/controllers/furimas_controller.rb
+++
@@ -1,2 +0,0 @@
-class FurimasController < ApplicationController
-end
--- test/controllers/furimas_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FurimasControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
