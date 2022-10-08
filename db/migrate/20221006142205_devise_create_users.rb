607d0747ed281762670f512e684f99700d4cfe3e
teiteipan06 <y7321543@gmail.com>
2022-10-08 19:03:31 +0900

    index viewsの追加

--- db/migrate/20221006142205_devise_create_users.rb
+++ db/migrate/20221006142205_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,              null: false, default: ""
+      t.string :email,                 null: false, default: ""
+      t.string :encrypted_password,    null: false, default: ""
+      t.string :first_name,            null: false, default: ""
+      t.string :last_name,             null: false, default: ""
+      t.string :first_name_kana,       null: false, default: ""
+      t.string :last_name_kana,        null: false, default: ""
+      t.date   :birthday,              null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,7 +38,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      
+      
       t.timestamps null: false
     end
 
