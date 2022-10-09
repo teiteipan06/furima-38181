80b84c36c34f40630063a4df5bc46e044a54a4e1
teiteipan06 <y7321543@gmail.com>
2022-10-09 12:19:08 +0900

    rspecの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :encrypted_password])
   end
 
 end
