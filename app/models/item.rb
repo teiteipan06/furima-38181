19457dfa0115e2ef134fa6b4b3940cf27e4b0044
teiteipan06 <y7321543@gmail.com>
2022-10-13 23:56:12 +0900

    itemのモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,20 @@ class Item < ApplicationRecord
 
   belongs_to :user
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_fee_id
+      validates :country_id
+      validates :days_to_ship_id
+    end
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
