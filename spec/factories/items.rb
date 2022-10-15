937110194488727510128bad096df80140f7741b
teiteipan06 <y7321543@gmail.com>
2022-10-15 16:49:11 +0900

    itemのfactoryBotの追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  {"neko"}
+    description           {"kawaii"}
+    category_id           {2}
+    status_id             {2}
+    shipping_fee_id       {2}
+    country_id            {2}
+    days_to_ship_id       {2}
+    price                 {1000}
+
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品を出品できるとき' do
+    context '商品を出品できるとき' do
+      it 'すべてのフォームの入力がされている' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品を出品できないとき' do
+      it 'imageが空では登録できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
 end
