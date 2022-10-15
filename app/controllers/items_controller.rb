be5196e9a59665349f042a6abc195c2eb56b951d
teiteipan06 <y7321543@gmail.com>
2022-10-15 19:30:25 +0900

    controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
 
 
   def index
-    @items = Item.all
   end
 
   def new
