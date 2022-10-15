2d2f5451ccf9b5dd4e2b7adc23fab3f04b251579
teiteipan06 <y7321543@gmail.com>
2022-10-15 10:44:40 +0900

    ImageMagickのインストール

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,26 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @items = Item.new 
   end
 
+  def create 
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def item_params
+    #params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_id, :country_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 
 
 end
