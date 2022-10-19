7f724ec9bdf796af8100002a0dac0894dbe8bd0b
teiteipan06 <y7321543@gmail.com>
2022-10-20 00:10:22 +0900

    controllerにbefore_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -24,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
@@ -48,6 +47,10 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 
 end
