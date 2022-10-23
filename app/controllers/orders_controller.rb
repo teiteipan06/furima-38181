511bb7244ea682afbb9bda638a29bb9f444665d1
teiteipan06 <y7321543@gmail.com>
2022-10-23 22:17:07 +0900

    購入ページの商品のデータを示すような設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :set_item
+
+
+  def index
+  end
+
+
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+  
 end
