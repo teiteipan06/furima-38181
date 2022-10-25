7c7c94edfdc498bc06d529c46623a03a22a745bf
teiteipan06 <y7321543@gmail.com>
2022-10-25 22:59:55 +0900

    orderのviewsの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,13 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 
@@ -14,4 +21,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def order_params
+    params.require(:order_destination).permit(:post_code, :country_id, :city, :banchi, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
 end
